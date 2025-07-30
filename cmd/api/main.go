65cb0996e2f84d065555dc6a6bab4a662a1dc18c
unknown <gabrielleite03@gmail.com>
2025-07-30 11:19:47 -0300

    testing

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,42 +2,39 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
+	"fmt"
 	"os"
+
+	"koto.com/internal/core/ports"
+	"koto.com/service"
 )
 
 func main() {
 	s := bufio.NewScanner(os.Stdin)
-
-	// allOps := []*[]models.CapitalGains{}
+	var outs []string
 	for s.Scan() {
 
-		// text := s.Text()
-		// capitalGainService := service.NewCapitalGainService(ports.NewStockService())
-		// if text == "exit" {
-		// 	break
-		// }
-		// if text == "quit" {
-		// 	break
-		// }
+		text := s.Text()
+		if text == "" {
+			break
+		}
+		capitalGainService := service.NewCapitalGainService(ports.NewStockService())
+		if text == "exit" {
+			break
+		}
+		if text == "quit" {
+			break
+		}
 
-		// cps, err := capitalGainService.GetCapitalGain(text)
+		cps, _ := capitalGainService.GetCapitalGain(text)
+		out, _ := json.Marshal(cps)
+		outs = append(outs, string(out))
 
-		// if err != nil {
-		// 	if err.Error() == "input string is empty" {
-		// 		for _, ops := range allOps {
-		// 			out, _ := json.Marshal(ops)
-		// 			fmt.Println(string(out))
+	}
 
-		// 		}
-		// 		os.Exit(0)
-
-		// 	}
-		// 	// panic(err)
-		// }
-		// allOps = append(allOps, cps)
-
-		continue
-
+	for _, row := range outs {
+		fmt.Println(row)
 	}
 
 }
